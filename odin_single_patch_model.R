69e6af3db4409927711892a50d713c0a33224e45
AmyDighe <amydighe@gmail.com>
2021-06-30 22:07:00 +0100

    minor changes to commenting round ageing

--- odin_single_patch_model.R
+++ odin_single_patch_model.R
@@ -115,7 +115,6 @@ p_I2[1:N_age] <- 1 - exp(- (gamma + mu[i]))
 p_R2[1:N_age] <- 1 - exp(- (sigma + mu[i])) 
 
 # outflows due to infection, recovery or death
-# (ageing within a disease state is dealt with separately)
 outflow_Sm[1:N_age] <- rbinom(Sm[i], prob = p_Sm[i])
 outflow_S[1:N_age] <- rbinom(S[i], prob = p_S[i])
 outflow_I[1:N_age] <- rbinom(I[i], prob = p_I[i])
@@ -123,18 +122,6 @@ outflow_R[1:N_age] <- rbinom(R[i], prob = p_R[i])
 outflow_S2[1:N_age] <- rbinom(S2[i], prob = p_S2[i])
 outflow_I2[1:N_age] <- rbinom(I2[i], prob = p_I2[i])
 outflow_R2[1:N_age] <- rbinom(R2[i], prob = p_R2[i])
-
-# # outflows due to ageing within a disease state
-# # when camels are +4 they remain in class 49 until they die
-# 
-# aged_Sm[1:(N_age - 1)] <- if(tt %% 30 == 0) Sm[i] - outflow_Sm[i] else 0
-# aged_S[1:(N_age - 1)] <- if(tt %% 30 == 0) S[i] - outflow_S[i] else 0
-# aged_I[1:(N_age - 1)] <- if(tt %% 30 == 0) I[i] - outflow_I[i] else 0
-# aged_R[1:(N_age - 1)] <- if(tt %% 30 == 0) R[i] - outflow_R[i] else 0
-# aged_S2[1:(N_age - 1)] <- if(tt %% 30 == 0) S2[i] - outflow_S2[i] else 0
-# aged_I2[1:(N_age - 1)] <- if(tt %% 30 == 0) I2[i] - outflow_I2[i] else 0
-# aged_R2[1:(N_age - 1)] <- if(tt %% 30 == 0) R2[i] - outflow_R2[i] else 0
-
 
 
 ###############################################################################################################
@@ -205,17 +192,13 @@ new_Sm[2:N_age] <- Sm[i] - outflow_Sm[i]
 new_S[1] <- S[1] - outflow_S[1] + new_waned_mAb[1] + births_not_protected 
 new_S[2:N_age] <- S[i] - outflow_S[i] + new_waned_mAb[i]
 
-#25 also has imported cases going in either daily or on a set day (day set to not coincide with ageing)
 new_I[1:24] <-  I[i] - outflow_I[i] + new_infections[i] + new_infections_mAb[i]
 new_I[25] <- I[i] - outflow_I[i] + new_infections[i] + new_infections_mAb[i] + imported_cases
 new_I[26:N_age] <-  I[i] - outflow_I[i] + new_infections[i] + new_infections_mAb[i]
 
 new_R[1:N_age] <- R[i] - outflow_R[i] + new_recoveries[i]
-
 new_S2[1:N_age] <- S2[i] - outflow_S2[i] + new_waned[i] + new_waned_2[i]
-
 new_I2[1:N_age] <- I2[i] - outflow_I2[i] + new_reinfections[i]
-
 new_R2[1:N_age] <- R2[i] - outflow_R2[i] + new_recoveries_2[i]
 
 
@@ -229,7 +212,6 @@ update(S[1]) <- if(tt %% 30 == 0) 0 else new_S[1]
 update(S[2:48]) <- if(tt %% 30 == 0) new_S[i - 1] else new_S[i]
 update(S[N_age]) <- if(tt %% 30 == 0) new_S[i - 1] + new_S[i] else new_S[i]
 
-#? also has imported cases going in either daily or on a set day (day set to not coincide with ageing)
 update(I[1]) <-  if(tt %% 30 == 0) 0 else new_I[1]
 update(I[2:24]) <- if(tt %% 30 == 0) new_I[i - 1] else new_I[i]
 update(I[25]) <- if(tt %% 30 == 0) new_I[i - 1] else if(tt == imp_t) 1 + new_I[i] else new_I[i]
@@ -271,7 +253,7 @@ update(seropoz_A4) <- (sum(S2[N_age]) + sum(I[N_age]) + sum(I2[N_age]) + sum(R[N
 ## initial states
 
 initial(Sm[1:N_age]) <- 0
-initial(S[1:N_age]) <- S_ini_p[i] # will be user-defined
+initial(S[1:N_age]) <- S_ini_p[i]
 initial(I[1:N_age]) <- 0
 initial(R[1:N_age]) <- 0
 initial(S2[1:N_age]) <- 0
@@ -314,7 +296,6 @@ S_ini[N_age] <- sum(cohort_remaining[1:a_max])
 
 # getting proportion of animals in each age-class, multiplying by N_0 and rounding to whole animals
 S_ini_p[1:N_age] <- round((S_ini[i] / sum(S_ini[1:N_age])) * N_0)
-
 
 
 ################################################################################################################################
@@ -413,14 +394,6 @@ dim(new_waned) <- N_age
 dim(new_reinfections) <- N_age
 dim(new_recoveries_2) <- N_age
 dim(new_waned_2) <- N_age
-
-# dim(aged_Sm) <- 48
-# dim(aged_S) <- 48
-# dim(aged_I) <- 48
-# dim(aged_R) <- 48
-# dim(aged_S2) <- 48
-# dim(aged_I2) <- 48
-# dim(aged_R2) <- 48
 
 dim(outflow_Sm) <- N_age
 dim(outflow_S) <- N_age
