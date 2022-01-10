d52c4f9dfeffa4a7a02d47b9adb0f52f69519d4f
AmyDighe <amydighe@gmail.com>
2022-01-10 15:45:13 +0000

    updaated to run for longer allowing equilibrum

--- R/odin_single_patch_persistence_estab_period.R
+++ R/odin_single_patch_persistence_estab_period.R
@@ -28,7 +28,7 @@ mu_4th_yr <- 0.0003603 # death rate for 4th year of life
 mu_adult_over_4 <- 0.0003603 # death rate in adulthood (>4 years)
 
 # input the time period that you wish to run the model for (in days)
-time_period <- 8300
+time_period <- 19710
 t <- seq(0:time_period)
 
 # set importation rate for introducing infectious individuals
@@ -48,16 +48,18 @@ ind1 <- rep(ind1, 4)
 ind2 <- rep(ind2, 4)
 
 estab <- vector(length = (dim(par_grid)[1]))
+m_persist <- estab
 persist <- estab
 period_av <- estab
 period_sd <- estab
-
+period_med <- estab
 
 for(i in 1:(dim(par_grid)[1])){
   
   imp_t <- par_grid$import_time[i] + (360 * seq(0,4, by = 1))
-  e_t <- imp_t[5] + (3*360)
-  p_t <- imp_t[5] + (18*360)
+  e_t <- imp_t[5] + (3 * 360)
+  m_t <- imp_t[5] + (25 * 360)
+  p_t <- imp_t[5] + (50 * 360)
   
   x <- sir_model$new(alpha = alpha, beta = par_grid$beta[i], gamma = gamma, 
                      sigma = par_grid$waning[i], sigma_m = sigma_m, Ab_susc = Ab_susc, mAb_susc = mAb_susc, 
@@ -69,6 +71,7 @@ for(i in 1:(dim(par_grid)[1])){
   out <- as.data.frame(replicate(100, x$run(t)[, 403]))
   
   estab[i] <- sum(out[e_t,] > 0)
+  m_persist[i] <- sum(out[m_t, ] > 0)
   persist[i] <- sum(out[p_t, ] > 0)
   
   if(persist[i] > 0){
@@ -79,33 +82,34 @@ for(i in 1:(dim(par_grid)[1])){
     
     if(persist[i] > 1){
       for(j in 1:(persist[i])){
-        ACF <- acf(out_persist[e_t:p_t, j], lag.max = 360*5, plot = FALSE)
+        ACF <- acf(out_persist[m_t:p_t, j], lag.max = 360*5, plot = FALSE)
         acf_df <- data.frame(acf = ACF$acf[100:(360*5)], lag = ACF$lag[100:(360*5)])
         period[j] <- acf_df[which.max(acf_df$acf), ]$lag 
       } 
       period_av[i] <- mean(period)
       period_sd[i] <- sd(period)
+      period_med[i] <- median(period)
       
     } else if(persist[i] == 1) {
-      ACF <- acf(out_persist[e_t:p_t], lag.max = 360*5, plot = FALSE)
+      ACF <- acf(out_persist[m_t:p_t], lag.max = 360*5, plot = FALSE)
       acf_df <- data.frame(acf = ACF$acf[100:(360*5)], lag = ACF$lag[100:(360*5)])
       period <- acf_df[which.max(acf_df$acf), ]$lag 
       
       period_av[i] <- period
       period_sd[i] <- NA
+      period_med[i] <- period
     }
 
   } else {
     period_av[i] <- NA
     period_sd[i] <- NA
+    period_med[i] <- NA
   }
   print(i)
 }
 
-saveRDS(file = "results/persist_estab_period_sp.rds", object = data.frame(persist = persist, estab = estab,
-                                                                  period_av = period_av, period_sd = period_sd))
-p <- readRDS("results/period.rds")
-q <- readRDS("results/persist.rds")
+ saveRDS(file = "results/persist_estab_period_sp.rds", object = data.frame(persist = persist, m_persist = m_persist, estab = estab,
+                                                                  period_av = period_av, period_sd = period_sd, period_med = period_med))
 d <- readRDS("results/persist_estab_period_sp.rds")
 results <- cbind(d, par_grid)
 results <- results %>% mutate(persist_estab = 100 * persist/estab)
@@ -159,7 +163,7 @@ ggsave(filename = "figs/estab5.png")
 # run with more fine population classes
 
 # periodicity
-ggplot(results %>% filter(import_time == 151))+
+ggplot(results %>% filter(import_time == 151) %>% filter(persist>50))+
   geom_point(aes(x = log10(pop), y = period_av/360, col = as.character(beta)), size = 4)+
   scale_color_viridis_d(name = "beta", alpha = 0.5)+
   facet_grid(waning~seasonality, 
@@ -173,13 +177,41 @@ ggplot(results %>% filter(import_time == 151))+
   theme(text = element_text(size = 20))+
   xlab("log(population size)")+
   ylab("period (years)")
-ggsave(filename = "figs/periodicity.png")
+ggsave(filename = "figs/mean_periodicity.png")
+
+ggplot(results %>% filter(import_time == 151) %>% filter(persist>50))+
+  geom_point(aes(x = log10(pop), y = period_med/360, col = as.character(beta)), size = 4)+
+  scale_color_viridis_d(name = "beta", alpha = 0.5)+
+  facet_grid(waning~seasonality, 
+             labeller = labeller(waning = waning.labs, seasonality = seasonality.labs))+
+  theme_minimal()+
+  geom_hline(yintercept = 1, col = "red", lty = 2)+
+  geom_hline(yintercept = 2, col = "red", lty = 2)+
+  theme(text = element_text(size = 20))+
+  xlab("log(population size)")+
+  ylab("period (years)")
+ggsave(filename = "figs/median_periodicity.png")
+
+
+
+
+
+
+
+
+
+
+
 
 
 ## getting some examples out
+results$id <- 1:360
+results %>% filter(pop == 1000000,
+                   beta == 0.25, seasonality == 0,
+                   import_time == 151)
 
 for(i in 1:(dim(par_grid)[1])){
-  i = 48
+  i = 109
   
   imp_t <- par_grid$import_time[i] + (360 * seq(0,4, by = 1))
   e_t <- imp_t[5] + (3*360)
@@ -205,12 +237,13 @@ for(i in 1:(dim(par_grid)[1])){
     
     if(persist[i] > 1){
       for(j in 1:(persist[i])){
-        ACF <- acf(out_persist[e_t:p_t, j], lag.max = 360*5, plot = FALSE)
+        ACF <- acf(out_persist[6840:p_t, j], lag.max = 360*5, plot = F)
         acf_df <- data.frame(acf = ACF$acf[100:(360*5)], lag = ACF$lag[100:(360*5)])
         period[j] <- acf_df[which.max(acf_df$acf), ]$lag 
       } 
       period_av[i] <- mean(period)
       period_sd[i] <- sd(period)
+      period_med[i] <- median(period)
       
     } else if(persist[i] == 1) {
       ACF <- acf(out_persist[e_t:p_t], lag.max = 360*5, plot = TRUE)
@@ -228,5 +261,7 @@ for(i in 1:(dim(par_grid)[1])){
   print(i)
 }
 
+out_persist$step <- t
 
-matplot(out, lyt = 2)
+matplot(y = out[m_t:p_t,1:5], type = "l",
+        lty = 1, lwd = 1.5)
