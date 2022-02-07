d5bf2b694f40621a4905f38233deadb8ed481ea5
AmyDighe <amydighe@gmail.com>
2022-02-07 11:20:31 +0000

    got ris of dependency on metapop test

--- R/dust_meta_error.R
+++ R/dust_meta_error.R
@@ -37,7 +37,7 @@ correction_ex <- matrix(c(2, 3, 3, 3, 2,
 n_particles <- 100L
 
 ## stochastic initialization
-S_ini_p <- stoch_init(alpha, delta = par_grid_metapop_test$seasonality[1], N_0 = 40,
+S_ini_p <- stoch_init(alpha, delta = 1, N_0 = 40,
                       mu, N_age, n_r = n_r, n_c = n_c)
 
 msirs_model <- msirs_meta_dust$new(pars = list(N_age = N_age, n_r = n_r, n_c = n_c, 
